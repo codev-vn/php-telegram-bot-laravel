543f498ab43d674a2b0929526b6b0ec330227778
Phong Tran <phong.tran@codev.vn>
2021-07-15 21:11:58 +0700

    Commit Database

--- src/database/migrations/2021_04_18_193565_create_poll_answer_table.php
+++ src/database/migrations/2021_04_18_193565_create_poll_answer_table.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCallbackQueryTable extends Migration
+class CreatePollAnswerTable extends Migration
 {
     public function up()
     {
